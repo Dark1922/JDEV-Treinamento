1cc57bafe6acf6ffae5f5405a0cff9f77a3cb409
dark1922 <jvdematos004@gmail.com>
2021-05-23 17:53:57 -0300

    for

---
+++ src/mod7/repFor.java
@@ -0,0 +1,13 @@
+package mod7;
+
+public class repFor {
+	public static void main(String[] args) {
+		
+		
+		for (int numero = 10; numero >= 1; numero --) {
+			
+			System.out.println("Numero: " + numero);
+		}
+	}
+
+}
