3e683d3cb3f9e4f571ec6d50fc91d6a68c6c088a
dark1922 <jvdematos004@gmail.com>
2021-05-23 16:50:51 -0300

    feat:opRelacionais

---
+++ src/mod7/opRelacionais.java
@@ -0,0 +1,20 @@
+package mod7;
+
+public class opRelacionais { 
+	
+	public static void main(String[] args) {
+		// <= >= > < != == 
+		int nota1 = 91;
+		int nota2 = 90;
+		
+		if (nota1 > nota2) {
+			System.out.println("a nota 1 é maior que a 2");
+			
+		}else {
+			System.out.println("as notas são diferentes");
+		}
+		
+	}
+
+}
+ 
