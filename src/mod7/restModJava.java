76ba24e21f9d22eb66eb33bdfbfd8304562251fe
dark1922 <jvdematos004@gmail.com>
2021-05-23 18:45:27 -0300

    feat:Mod resto da div

---
+++ src/mod7/restModJava.java
@@ -0,0 +1,14 @@
+package mod7;
+
+public class restModJava {
+	
+	public static void main(String[] args) {
+		/*Módulo resto da divisão % */
+		
+		double carro = 9;
+		double pessoas = 2;
+		double resto = carro % pessoas; //soma o resto da divisão
+		System.out.println("Sobraram exatamente: " + resto + " carro");
+	}
+
+}
