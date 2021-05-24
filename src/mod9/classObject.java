1420cd4e8dc175e136347ddbf61e8d921423f086
dark1922 <jvdematos004@gmail.com>
2021-05-24 18:35:23 -0300

    feat: getters and setters

--- src/mod9/classObject.java
+++ src/mod9/classObject.java
@@ -3,16 +3,16 @@ package mod9;
 public class classObject {
 	
 		
-		String nome; //nome é um texto ent string
-		int idade;
-		String dataNascimento;
-		String registroGeral;
-		String numeroCpf;
-		String nomeMae;
-		String nomePai;
-		String dataMatricula;
-		String nomeEscola;
-		String serieMatriculado;
+		private String nome; //nome é um texto ent string
+		private int idade; //esses são os atributos  do aluno
+		private String dataNascimento;
+		private String registroGeral;
+		private String numeroCpf;
+		private String nomeMae;
+		private String nomePai;
+		private String dataMatricula;
+		private String nomeEscola;
+		private String serieMatriculado;
 		
 	   public classObject() {
 		   //cria os dados na memoria - sendo padrão do java
@@ -25,6 +25,70 @@ public class classObject {
 	   public classObject(String nomePadrao, int idadePadrao) {
 		   nome = nomePadrao; //o nome é igual ao nome padrao string
 		   idade = idadePadrao;
+	   }
+	   
+	   public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	/*Veremos os metodos SETTERS E GETTERS do objeto*/
+	   //Recebe dados 
+	   public void setNome(String nome) {
+		   this.nome = nome;
+	   }
+	   
+	   public String getNome() {
+		   return nome; //pode receber o nome sem o this
 	   }
 
 }
--- src/mod9/objectContrutor.java
+++ src/mod9/objectContrutor.java
@@ -3,21 +3,38 @@ package mod9;
 import mod9.classObject;
 public class objectContrutor {
 	
-	//objeto ainda n existe na memoria
-	classObject aluno;
+	public static void main(String[] args) {
+		
 	
-	//Agora temos um objeto real na memoria
-	//um objeto representa um mundo real
-	//ele pega todas classes do msm package ou outro
-	//e instancia aqui com suas classes massa
-	classObject aluno1 = new classObject();
+	classObject aluno = new classObject();//crio 1 objeto
+    aluno.setNome("joao");
+    aluno.setDataMatricula("07/2018");
+    aluno.setDataNascimento("24/10/1999");
+    aluno.setIdade(21);
+    aluno.setNomeEscola("UDF-Universidade do distrito federa");
+    aluno.setNomeMae("marcia");
+    aluno.setNumeroCpf("05821275130");
+    aluno.setRegistroGeral("3.597.011");
+    aluno.setSerieMatriculado("20085435");
+    aluno.setNomePai("ugauga");
+    
+    System.out.println("Nome é: " + aluno.getNome());
+    System.out.println("Idade: " + aluno.getIdade());
+    System.out.println("Nome da facu: " + aluno.getNomeEscola());
+    System.out.println("Nome mae: " + aluno.getNomeMae());
+    System.out.println("Numero cpf: " + aluno.getNumeroCpf());
+    System.out.println("RGM: " + aluno.getSerieMatriculado());
+    System.out.println("paio: " + aluno.getNomePai());
+    System.out.println("Data matricula: " + aluno.getDataMatricula());
+    System.out.println("Data nascimento: " + aluno.getDataNascimento());
+    System.out.println("RGM: " + aluno.getRegistroGeral());
 	
-	classObject aluno2 = new classObject();//construtor padrao
+	classObject aluno2 = new classObject();
 	
 	classObject aluno3 = new classObject("joao");
-	classObject aluno4 = new classObject("john", 10);//construtor com parametro
+	classObject aluno4 = new classObject("john", 10);
 	
 	//classObject aluno5 = new  
 	
-
+	}
 }
