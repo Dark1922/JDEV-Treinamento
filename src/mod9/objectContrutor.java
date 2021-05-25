f168f33a2a924df79c22d1490cc9256c3dcd5bfb
dark1922 <jvdematos004@gmail.com>
2021-05-25 20:45:31 -0300

    soma de lista da media de um aluno

--- src/mod9/objectContrutor.java
+++ src/mod9/objectContrutor.java
@@ -18,17 +18,7 @@ public class objectContrutor {
    String nomePai = JOptionPane.showInputDialog("Nome de seu pai");
    String nomeEscola = JOptionPane.showInputDialog("Nome de sua instituição");
    
-   String disciplina1 = JOptionPane.showInputDialog("Disciplina 1?");
-	 String nota1 = JOptionPane.showInputDialog("nota1");
-	 
-	 String disciplina2 = JOptionPane.showInputDialog("Disciplina 2?");
-	 String nota2 = JOptionPane.showInputDialog("nota2");
-	 
-	 String disciplina3 = JOptionPane.showInputDialog("Disciplina 3?");
-	 String nota3 = JOptionPane.showInputDialog("nota3");
-	 
-	 String disciplina4 = JOptionPane.showInputDialog("Disciplina 4?");
-	 String nota4 = JOptionPane.showInputDialog("nota4");
+
    
 	classObject aluno = new classObject();//crio 1 objeto
 
@@ -44,16 +34,32 @@ public class objectContrutor {
     aluno.setSerieMatriculado(serieMatriculado);
     aluno.setNomePai(nomePai);
     
+    //lista da disciplina gerada
+	Disciplina disciplina = new Disciplina();
+	disciplina.setDisciplina("Banco de dados");//nome da disciplina
+	disciplina.setNota(90);//criado na nossa classe disciplina , valor da nota
 	
-    aluno.getDisciplina().setNota1(Double.parseDouble(nota1));//ele recebe uma string e retorna um double
-    aluno.getDisciplina().setNota2(Double.parseDouble(nota2));
-    aluno.getDisciplina().setNota3(Double.parseDouble(nota3));
-    aluno.getDisciplina().setNota4(Double.parseDouble(nota4));
-	//passa a disciplina com os dados de do calculo de media e com as diciplina tb massa
-    aluno.getDisciplina().setDisciplina1(disciplina1);
-    aluno.getDisciplina().setDisciplina2(disciplina2);
-    aluno.getDisciplina().setDisciplina3(disciplina3);
-    aluno.getDisciplina().setDisciplina4(disciplina4);
+	
+	//instaciou e criou os elemento da lista massa 
+	Disciplina disciplina2 = new Disciplina();
+	disciplina2.setDisciplina("Matematica");
+	disciplina2.setNota(80);
+
+	
+	Disciplina disciplina3 = new Disciplina();
+	disciplina3.setDisciplina("Nodejs");
+	disciplina3.setNota(88);
+
+	
+	Disciplina disciplina4 = new Disciplina();
+	disciplina4.setDisciplina("Engles");
+	disciplina4.setNota(50);
+
+	
+	aluno.getDisciplinas().add(disciplina);
+	aluno.getDisciplinas().add(disciplina2);//adiciono pra lista
+	aluno.getDisciplinas().add(disciplina3);
+	aluno.getDisciplinas().add(disciplina4);
 	
 	System.out.println(aluno.toString());
 	System.out.println("Média da nota é: " + aluno.getMedia());
@@ -61,34 +67,6 @@ public class objectContrutor {
 	System.out.println("Resultado metodo1: " + aluno.getAlunoAprovado1());
     
 	
-	
-	
-   /* //System.out.println("Nome é: " + aluno.getNome());
-    //System.out.println("Idade: " + aluno.getIdade());
-    //System.out.println("Nome da facu: " + aluno.getNomeEscola());
-    //System.out.println("Nome mae: " + aluno.getNomeMae());
-    //System.out.println("Numero cpf: " + aluno.getNumeroCpf());
-    //System.out.println("RGM: " + aluno.getSerieMatriculado());
-    //System.out.println("paio: " + aluno.getNomePai());
-    //System.out.println("Data matricula: " + aluno.getDataMatricula());
-    //System.out.println("Data nascimento: " + aluno.getDataNascimento());
-    //System.out.println("RGM: " + aluno.getRegistroGeral());*/
-    
-	/*//System.out.println("----------------------------------------------");
-	
-	//classObject aluno2 = new classObject();
-	//aluno2.setNome("hellow baby");
-	//aluno2.setIdade(99);
-	//System.out.println(aluno2.getNome());
-	//System.out.println(aluno2.getIdade());*/
-	
-	//classObject aluno3 = new classObject("joao");
-	//classObject aluno4 = new classObject("john", 10);
-	
-	//classObject aluno5 = new  
-		
-
-		//System.out.println(aluno.toString());//descrição do objeto ná memoria
 		
 
 	  
