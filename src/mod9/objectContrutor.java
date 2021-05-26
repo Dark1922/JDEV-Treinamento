57b6df6d160b13c826cf44b19b39e074a6175652
dark1922 <jvdematos004@gmail.com>
2021-05-25 21:11:25 -0300

    dados dinamicos de disciplinas e notas

--- src/mod9/objectContrutor.java
+++ src/mod9/objectContrutor.java
@@ -3,72 +3,51 @@ package mod9;
 import javax.swing.JOptionPane;
 
 import mod9.classObject;
+
 public class objectContrutor {
-	
+
 	public static void main(String[] args) {
-		
-   String nome = JOptionPane.showInputDialog("Qual o nome do aluno");
-   String idade = JOptionPane.showInputDialog("Digite sua idade");
-   String dataNascimento = JOptionPane.showInputDialog("Data de nascimento");
-   String dataMatricula = JOptionPane.showInputDialog("Data matricula");
-   String numeroCpf = JOptionPane.showInputDialog("numero cpf");
-   String registroGeral = JOptionPane.showInputDialog("RGM");
-   String serieMatriculado = JOptionPane.showInputDialog("Matricula");
-   String nomeMae = JOptionPane.showInputDialog("Nome de sua mae");
-   String nomePai = JOptionPane.showInputDialog("Nome de seu pai");
-   String nomeEscola = JOptionPane.showInputDialog("Nome de sua instituição");
-   
 
-   
-	classObject aluno = new classObject();//crio 1 objeto
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno");
+		String idade = JOptionPane.showInputDialog("Digite sua idade");
+		String dataNascimento = JOptionPane.showInputDialog("Data de nascimento");
+		String dataMatricula = JOptionPane.showInputDialog("Data matricula");
+		String numeroCpf = JOptionPane.showInputDialog("numero cpf");
+		String registroGeral = JOptionPane.showInputDialog("RGM");
+		String serieMatriculado = JOptionPane.showInputDialog("Matricula");
+		String nomeMae = JOptionPane.showInputDialog("Nome de sua mae");
+		String nomePai = JOptionPane.showInputDialog("Nome de seu pai");
+		String nomeEscola = JOptionPane.showInputDialog("Nome de sua instituição");
 
-	
-    aluno.setNome(nome);
-    aluno.setDataMatricula(dataMatricula);
-    aluno.setDataNascimento(dataNascimento);
-    aluno.setIdade(Integer.valueOf(idade));//vai transforma idade em um numero inteiro
-    aluno.setNomeEscola(nomeEscola);
-    aluno.setNomeMae(nomeMae);
-    aluno.setNumeroCpf(numeroCpf);
-    aluno.setRegistroGeral(registroGeral);
-    aluno.setSerieMatriculado(serieMatriculado);
-    aluno.setNomePai(nomePai);
-    
-    //lista da disciplina gerada
-	Disciplina disciplina = new Disciplina();
-	disciplina.setDisciplina("Banco de dados");//nome da disciplina
-	disciplina.setNota(90);//criado na nossa classe disciplina , valor da nota
-	
-	
-	//instaciou e criou os elemento da lista massa 
-	Disciplina disciplina2 = new Disciplina();
-	disciplina2.setDisciplina("Matematica");
-	disciplina2.setNota(80);
+		classObject aluno = new classObject();// crio 1 objeto
 
-	
-	Disciplina disciplina3 = new Disciplina();
-	disciplina3.setDisciplina("Nodejs");
-	disciplina3.setNota(88);
+		aluno.setNome(nome);
+		aluno.setDataMatricula(dataMatricula);
+		aluno.setDataNascimento(dataNascimento);
+		aluno.setIdade(Integer.valueOf(idade));// vai transforma idade em um numero inteiro
+		aluno.setNomeEscola(nomeEscola);
+		aluno.setNomeMae(nomeMae);
+		aluno.setNumeroCpf(numeroCpf);
+		aluno.setRegistroGeral(registroGeral);
+		aluno.setSerieMatriculado(serieMatriculado);
+		aluno.setNomePai(nomePai);
 
-	
-	Disciplina disciplina4 = new Disciplina();
-	disciplina4.setDisciplina("Engles");
-	disciplina4.setNota(50);
+		// pos = posição
+		for (int pos = 1; pos <= 4; pos++) {// vai ficar dinamico o numero da disciplina que vai aparecer
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + pos + " ?");
+			String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + pos + " ?");
 
-	
-	aluno.getDisciplinas().add(disciplina);
-	aluno.getDisciplinas().add(disciplina2);//adiciono pra lista
-	aluno.getDisciplinas().add(disciplina3);
-	aluno.getDisciplinas().add(disciplina4);
-	
-	System.out.println(aluno.toString());
-	System.out.println("Média da nota é: " + aluno.getMedia());
-	System.out.println("Resultado: " + (aluno.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-	System.out.println("Resultado metodo1: " + aluno.getAlunoAprovado1());
-    
-	
-		
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
 
-	  
+			aluno.getDisciplinas().add(disciplina);// adiciona pra lista add
+		} // ele recebe um double ent Double.valueOf
+
+		System.out.println(aluno.toString());
+		System.out.println("Média da nota é: " + aluno.getMedia());
+		System.out.println("Resultado: " + (aluno.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		System.out.println("Resultado metodo1: " + aluno.getAlunoAprovado1());
+
 	}
 }
