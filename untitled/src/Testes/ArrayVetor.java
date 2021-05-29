3c27ce956d93d9cb905353e39fafb9d86f90785a
dark1922 <jvdematos004@gmail.com>
2021-05-28 22:05:10 -0300

    lista dinamica

--- untitled/src/Testes/ArrayVetor.java
+++ untitled/src/Testes/ArrayVetor.java
@@ -1,21 +1,25 @@
 package Testes;
 
+import javax.swing.*;
+
 public class ArrayVetor {
            //método executavel
    public static void main(String[] args) {
-     //array pode ser de todos tipo de dados e objetos tb
 
-     //Array sempre deve ter a quantidade de posições definidas
-     double[] notas = new double[4]; //array de notas do tipo double numeros
-     //instacia ele com o array com o numero da array que é obrigatorio 4 posições
+     String posicoes = JOptionPane.showInputDialog("Quantas posições deve ter o Array");
 
-     notas[0] = 9.9; //atribuir valor nas posições no valor do array
-     notas[1] = 5;
-     notas[2] = 8;
-     notas[3] = 10;
-   //enqt minha posição  for menor que as posições da array vai somando até
-     // as 4 posições dela
-   for(int i = 0; i < 4; i ++) {
+      //vai transforma string em numeros inteiros
+     double[] notas = new double[Integer.parseInt(posicoes)]; 
+
+  for (int i = 0; i < notas.length; i++) {
+     String valor = JOptionPane.showInputDialog("Qual valor da posição = " + i);
+     notas[i] = Double.valueOf(valor);//vai passar o valor que demos pra posição da array
+    //e pela quantidade de array que temos , passar o valor pra todas que declaramos
+
+  }
+
+//vai pecorrer o array até ser menor que o tamanho do array própio + dinamico
+   for(int i = 0; i < notas.length; i ++) {
        System.out.println("Nota " + (i + 1) + " é = " + notas[i]);
    }
  //nossa nota na posição da array que está sendo varrida
