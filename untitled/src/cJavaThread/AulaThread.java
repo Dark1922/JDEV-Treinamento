2882a040dbed1cb1ebc6b40a72491181f60863a3
dark1922 <jvdematos004@gmail.com>
2021-05-31 18:34:46 -0300

    Thread Runnable

--- untitled/src/cJavaThread/AulaThread.java
+++ untitled/src/cJavaThread/AulaThread.java
@@ -5,59 +5,60 @@ import javax.swing.*;
 public class AulaThread {
   public static void main(String[] args) throws InterruptedException {//lança essa exceção
 
-    new Thread() { //onde bota o run aqui dentro
+   Thread threadEmail = new Thread(thread1);
+   threadEmail.start();
+    /*******************************Divisão thread****************************************/
+    Thread thredEnviaFuncao = new Thread(thread2);
+    thredEnviaFuncao.start();
 
-      public  void run() {//codigo da rotina bota o código que agr quer executar
-        //código que eu quero executar da rotina em paralelo
-        for(int i = 0; i < 10;  i++) {
 
-          //queria enviar isso com um tempo de parada e com tempo de envio determinaddo
-          System.out.println("Executando algo " + (i + 1) + " vez ");
 
-          try {//jeito de capturar erro no thread
+      System.out.println("\n O CÓDIGO CHEGOU AO FIM ");
+
+    JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
+  }
+  private static Runnable thread2 = new Runnable() {
+    @Override
+    public void run() {
+      for(int i = 0; i < 10;  i++) {
+
+        //queria enviar isso com um tempo de parada e com tempo de envio determinaddo
+        System.out.println("Executando algo " + (i + 1) + " vez ");
+
+        try {//jeito de capturar erro no thread
 
           Thread.sleep(1000);
 
         }catch (InterruptedException e) {
 
-                 e.printStackTrace();
-          }
-
+          e.printStackTrace();
         }
 
       }
-    }.start();//start liga a thread que fica processando paralelamente por trás
-    //então os código abaixo em 1 segundo fica executando massa
+    }
+  };
 
-    /*******************************Divisão thread****************************************/
 
-    new Thread() { //Envio de email
+  private static Runnable thread1 = new Runnable() {
+    @Override
+    public void run() {
+      for(int i = 0; i < 10;  i++) {
 
-      public  void run() {//codigo da rotina bota o código que agr quer executar
-        //código que eu quero executar da rotina em paralelo
-        for(int i = 0; i < 10;  i++) {
+        //queria enviar isso com um tempo de parada e com tempo de envio determinaddo
+        System.out.println("Executando Envio de email " + (i + 1) + " Efetuado com sucesso ");
 
-          //queria enviar isso com um tempo de parada e com tempo de envio determinaddo
-          System.out.println("Executando Envio de email " + (i + 1) + " Efetuado com sucesso ");
+        try {//jeito de capturar erro no thread
 
-          try {//jeito de capturar erro no thread
+          Thread.sleep(2000);
 
-            Thread.sleep(1000);
+        }catch (InterruptedException e) {
 
-          }catch (InterruptedException e) {
-
-            e.printStackTrace();
-          }
-
+          e.printStackTrace();
         }
 
       }
-    }.start();
 
-
-    //Código do usuario continuo o fluxo do trabalho
-      System.out.println("\n O CÓDIGO CHEGOU AO FIM ");
-    //Fluxo do sistema cadastro de venda ou emissão de nota fiscal algo do tipo
-    JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
-  }
+    }
+  };
+  //private pq vai só trabalhar com ele nessa classe estatico por causa od método main
 }
